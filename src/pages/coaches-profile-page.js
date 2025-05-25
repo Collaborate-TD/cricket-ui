1ae4377d586048c389514c3d5908a513fe72effb
Maharshi2301 <mhvyas2001@gmail.com>
2025-05-24 23:19:14 -0400

    feat: Created Sign-in and Login page to register data in database.

---
+++ src/pages/coaches-profile-page.js
@@ -0,0 +1,21 @@
+// filepath: cricket-ui/src/pages/CoachProfile.js
+import React from "react";
+
+const CoachProfile = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Archit</h1>
+      <p>Coach</p>
+      <ul>
+        <li>Personal Information</li>
+        <li>Students</li>
+        <li>All Videos</li>
+        <li>All Pictures</li>
+        <li>Favourites</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CoachProfile;
---
+++ src/pages/student-profile-page.js
@@ -0,0 +1,21 @@
+// filepath: cricket-ui/src/pages/StudentProfile.js
+import React from "react";
+
+const StudentProfile = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Brian</h1>
+      <p>Student</p>
+      <ul>
+        <li>Personal Information</li>
+        <li>Coaches</li>
+        <li>All Videos</li>
+        <li>All Pictures</li>
+        <li>Drills</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default StudentProfile;
